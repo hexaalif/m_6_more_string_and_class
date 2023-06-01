deecfd68483e836479aa57d1c63920d0034a7ece
hexaalif <hexaalif2020@gmail.com>
2023-06-01 22:25:43 +0600

    working files

--- string_constructor.cpp
+++ string_constructor.cpp
@@ -5,7 +5,9 @@ using namespace std;
 int main()
 {
     // string s("hello world");
-    string s("hello world", 4);
+    // string s("hello world", 4);
+    string a = "hello world";
+    string s(a, 4);
 
     // cin>>s;
     cout << s << endl;
