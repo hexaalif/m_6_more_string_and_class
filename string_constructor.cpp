814a609d671895bdd67e2ebf68bc979116a9510f
hexaalif <hexaalif2020@gmail.com>
2023-06-01 22:22:49 +0600

    first commit

---
+++ string_constructor.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    // string s("hello world");
+    string s("hello world", 4);
+
+    // cin>>s;
+    cout << s << endl;
+
+    return 0;
+}
